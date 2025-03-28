ceb6dc7f71098859a2e911d4cfb2c235d82f9e45
Denis Katasonov <diarusfoxs@gmail.com>
2025-03-28 18:56:31 +0200

    Add hotel Room entity
    The room has the properties id, number of beds, price per day and bool variable whether the room is occupied or not.

---
+++ MultithreadingHotel/Model/HotelRoom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingHotel.Model
+{
+    class HotelRoom
+    {
+        private int roomId;
+        private int _nextRoomId;
+        private int sleepPlaces;
+        private int costPerDay;
+        private bool busy = false;
+    }
+}
