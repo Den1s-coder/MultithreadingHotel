20827a416d8ec4d51086eaa7b66a606bc12127a4
Denis Katasonov <diarusfoxs@gmail.com>
2025-04-03 21:16:31 +0300

    Add tourist entity

---
+++ MultithreadingHotel/Model/Tourist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingHotel.Model
+{
+    internal class Tourist
+    {
+        public int orderId;
+        public int touristCount;
+        public int orderedDays;
+
+        private int _nextId = 1;
+    }
+}
