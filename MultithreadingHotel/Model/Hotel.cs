7664b9c685a0c713d3aae8fc2c41fcfaf9af27a6
Denis Katasonov <diarusfoxs@gmail.com>
2025-04-04 17:15:44 +0300

    Add Hotel entity
    -hotel has number of rooms.
    -method StartTouristFlow creates new tourists and calls method TryCheckIn.
    -method TryCheckIn tries to check tourist into free room if there is one.

---
+++ MultithreadingHotel/Model/Hotel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingHotel.Model
+{
+    internal class Hotel
+    {
+        private List<HotelRoom> _rooms = new();
+        private object _lock = new object();
+
+        public Hotel(int roomCount) 
+        {
+            for (int i = 0; i < roomCount; i++) 
+            {
+                _rooms.Add(new HotelRoom(2, 100)); 
+            }
+        }
+
+        public void StartTouristFlow(int roomCount) 
+        {
+            Task.Run(() => {
+                Random random = new();
+
+                while (true)
+                {
+                    Thread.Sleep(random.Next(5000, 10000));
+
+                    Tourist tourist = new Tourist(2, 5);
+
+                    Task.Run(() => TryCheckIn(tourist));
+                }
+            });
+        }
+
+        private void TryCheckIn(Tourist tourist) 
+        {
+            lock (_lock) 
+            {
+                HotelRoom? freeRoom = _rooms.FirstOrDefault(r => !r.Busy && r.SleepPlaces >= tourist.TouristCount);
+                if (freeRoom != null)
+                {
+                    freeRoom.Busy = true;
+
+                    Task.Run(() =>
+                    {
+                        Thread.Sleep(tourist.OrderedDays * 1000);
+                        lock (_lock)
+                        {
+                            freeRoom.Busy = false;
+
+                        }
+                    });
+                }
+                else { }
+            }
+        }
+            
+    }
+}
