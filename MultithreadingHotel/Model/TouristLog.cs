a3042d4418d8b4e2f4b82bf2c84c6737c2b810cf
Denis Katasonov <diarusfoxs@gmail.com>
2025-04-07 13:07:06 +0300

    Add Tourist logs in JSON format
    -Create TouristLog entity which contains the necessary variables.
    -Add LogToJson method to the hotel which will create a JSON file and write logs to it.

---
+++ MultithreadingHotel/Model/TouristLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingHotel.Model
+{
+    public class TouristLog
+    {
+        public int TouristId { get; set; }
+        public int RoomId { get; set; }
+        public int Days { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime CheckOutTime { get; set; }
+    }
+}
